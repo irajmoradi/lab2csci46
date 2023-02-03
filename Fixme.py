09dc07adc8c0ad8d532e7b0124522cc5ddeb900f
Emma Munoz <epaisano24@cmc.edu>
2023-02-03 10:00:14 -0800

    fixed flake8 errors

--- Fixme.py
+++ Fixme.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
     '''
     xs = range(n+1)
-    xs = filter(lambda x: x%2 == 0, xs)
+    xs = filter(lambda x: x % 2 == 0, xs)
     xs = list(xs)
     return xs
